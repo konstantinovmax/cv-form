08a564f827d50e9af9a79003b1f8915d98c725b0
Maxim <konstantinovmax@gmail.com>
2021-08-07 15:54:24 +0300

    feat: модернизация модальных окон

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,26 @@
+import React from 'react';
 import './Modal.css';
 
 function Modal(props) {
+
+  function handleClickLayoutModalClose(e) {
+    if (e.target === e.currentTarget) {
+        props.onClose();
+    }
+  };
+
+  React.useEffect(() => {
+    function handleClickEscapeModalClose(e) {
+      if (e.key === 'Escape') {
+        props.onClose();
+      }
+    }
+    window.addEventListener('keydown', handleClickEscapeModalClose);
+    return () => window.removeEventListener('keydown', handleClickEscapeModalClose);
+  });
+
   return (
-    <div className={`modal modal_type_${props.name} ${props.isOpen && 'modal_is-open'}`}>
+    <div className={`modal modal_type_${props.name} ${props.isOpen && 'modal_is-open'}`} onClick={handleClickLayoutModalClose}>
       <div className={`modal__container modal__container_type_${props.name}`}>
         {props.children}
       </div>  
